b58ce7eb44b5366a07a2044b7f814572b5de9208
thanhba0 <43293701+thanhba0@users.noreply.github.com>
2019-10-17 23:15:43 +0700

    Add files via upload

---
+++ code_ThayTan/BTT1/MainPage.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace BT1
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked_2(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new MH_Chao());
+
+        }
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new MH_TinhTong());
+        }
+        private void Button_Clicked_1(object sender, EventArgs e)
+        {
+
+        }
+        private void cmdAbsoluteLayout_Clicked(object sender, EventArgs e)
+        {
+
+        }
+        private void cmdgridLayout_Clicked(object sender, EventArgs e)
+        {
+
+        }
+        private void cmdlogin_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new MH_Login());
+        }
+        private void cmdFlexLayout_Clicked(object sender, EventArgs e)
+        {
+
+        }
+        private void cmdScrollView_Clicked(object sender, EventArgs e)
+        {
+
+        }
+        private void cmdEntry_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void cmdListview1_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
