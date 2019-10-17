2612b70f91c65f6cf506c704b9159876bb94c9e1
thanhba0 <43293701+thanhba0@users.noreply.github.com>
2019-10-17 23:41:27 +0700

    update

---
+++ code_ThayTan/BTT1/MH_TinhTong.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace BT1
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class MH_TinhTong : ContentPage
+	{
+		public MH_TinhTong ()
+		{
+			InitializeComponent ();
+		}
+
+        private void cmdtinhtong_Clicked(object sender, EventArgs e)
+        {
+            double so1, so2, tong;
+            so1 = double.Parse(txtsoa.Text);
+            so2 = double.Parse(txtsob.Text);
+            tong = so1 + so2;
+            txttong.Text = tong.ToString();
+        }
+    }
+}
